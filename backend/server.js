10f32d0cfef189d0f14a1c32d143d92d807d82ca
KyleLavigne <112232301+KyleLavigne@users.noreply.github.com>
2025-12-08 19:53:36 -0600

    updated CORS

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN || "*",
+        credentials: true
+    })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/static", express.static("static"));
